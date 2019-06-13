1bd5f543b46b0fed7e6a42f7de34a06492dd67bc
Divya Suneeth <divyaprabhakar08@gmail.com>
2019-06-12 23:17:24 -0700

    Adding CURD opperations for Restaurant page

--- finalproject.py
+++ finalproject.py
@@ -1,42 +1,58 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 app= Flask(__name__)
 
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'},
-{'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},
-{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},
-{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},
-{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
 
 
 @app.route('/')
 @app.route('/restaurants')
 def showRestaurants():
+    restaurants= session.query(Restaurant).all()
     return render_template('restaurants.html',restaurants=restaurants) #"This page will show all my restaurants!!"
 
-@app.route('/restaurants/new')
+@app.route('/restaurants/new',methods=['GET','POST'])
 def newRestaurant():
-    return render_template('newrestaurant.html',restaurants=restaurants)#"This page will let you create a new restaurant"
+    if request.method=='POST':
+        newRestaurant=Restaurant(name=request.form['name'])
+        session.add(newRestaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('newrestaurant.html')#"This page will let you create a new restaurant"
 
-@app.route('/restaurants/<int:restaurant_id>/edit')
+@app.route('/restaurants/<int:restaurant_id>/edit',methods=['GET','POST'])
 def editRestaurant(restaurant_id):
-    return render_template('editrestaurant.html',restaurant_id=restaurant_id)#"This page will let you Edit a restaurant"
+    getRestaurant=session.query(Restaurant).filter_by(id=restaurant_id).one()
+    if request.method=='POST':
+        if request.form['name']:
+            getRestaurant.name=request.form['name']
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('editrestaurant.html',item=getRestaurant)#"This page will let you Edit a restaurant"
 
-@app.route('/restaurants/<int:restaurant_id>/delete')
+@app.route('/restaurants/<int:restaurant_id>/delete',methods=['GET','POST'])
 def deleteRestaurant(restaurant_id):
-    return render_template('deleterestaurant.html',restaurant_id=restaurant_id)#"This page will let you delete a restaurant"
+    getRestaurant=session.query(Restaurant).filter_by(id=restaurant_id).one()
+    if request.method=='POST':
+        session.delete(getRestaurant)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('deleterestaurant.html',item=getRestaurant)#"This page will let you delete a restaurant"
 
 @app.route('/restaurants/<int:restaurant_id>/menu')
 def showRestaurantMenu(restaurant_id):
-    return render_template('menu.html',restaurant_id=restaurant_id)#"This page will show the restaurant menu"
+    items= session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return render_template('menu.html',items=items) #"This page will show the restaurant menu"
 
 @app.route('/restaurants/<int:restaurant_id>/menu/new')
 def newRestaurantMenu(restaurant_id):
